4b7cd1acca562e3a55bfed43f5e0f4f479627eca
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 15:16:16 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-let table = document.getElementById("myTable");
-function createTable() {
-    let r = prompt("Input number of rows");
-    r = parseInt(r);
-    let c = prompt("Input number of columns");
-    c = parseInt(c);
-    for (let i = 0; i < r; i++ ){
-        let tr = document.createElement("tr");
-        for ( let j = 0; j  < c; j++){
-            let td = document.createElement("td");
-            td.innerText = `Row-${i} Column-${j}`;
-            tr.appendChild(td);
-        }
-        table.appendChild(tr);
-    }
-}
-  
+function insert_Row() {
+  // Get a reference to the table
+  var table = document.getElementById('sampleTable');
 
+  // Insert a new row at the top of the table
+  var newRow = table.insertRow(0);
+
+  // Insert two cells into the new row
+  var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+
+  // Set the content of the new cells
+  cell1.innerHTML = 'New Cell1';
+  cell2.innerHTML = 'New Cell2';
+}
