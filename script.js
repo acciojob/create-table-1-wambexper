8dcca507e49bce691cfba375b3a4f78f5a073e4f
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 15:11:22 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+let table = document.getElementById("myTable");
+function createTable() {
+    let r = prompt("Input number of rows");
+    r = parseInt(r);
+    let c = prompt("Input number of columns");
+    c = parseInt(c);
+    for (let i = 0; i < r; i++ ){
+        let tr = document.createElement("tr");
+        for ( let j = 0; j  < c; j++){
+            let td = document.createElement("td");
+            td.innerText = `Row-${i} Column-${j}`;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
 }
+  
+
